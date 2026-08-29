9ade8daf1c5cd145f9435bcf9a9fb536e7d19c76
Florian Hofmann <hofmann_florian@outlook.com>
2026-08-29 14:44:23 +0200

    Configure PWA for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/campaign-guide/',
+  
   plugins: [
     react(),
 
@@ -26,22 +28,22 @@ export default defineConfig({
 
         display: 'standalone',
 
-        start_url: '/',
-        scope: '/',
+        start_url: '/campaign-guide/',
+        scope: '/campaign-guide/',
 
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/campaign-guide/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/campaign-guide/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/campaign-guide/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
